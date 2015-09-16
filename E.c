de03d42d3ce60e3656409e63ff59990ebab79882
Rafael <rtrindade@inf.ufsm.br>
2015-09-16 00:47:21 -0300

    bissecao

--- E.c
+++ E.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
 
 int main() {
-  long int b;
+  long int b, esq = 1, dir, pto,ant;
   long int n,i,p;
   scanf("%ld %ld",&n,&b);
-
-  p = b/(n*2);
-  i = p*2+1;
-  //printf("b %ld - %ld\n",b,4*(p*n + (n/2 + n%2)*(n/2 + n%2) - (n-p)*(n-p)));
-  b -= 4*(p*n + (n/2 + n%2)*(n/2 + n%2) - (n-p)*(n-p));
-  //for(i=1,p=0;b>0;i+=2,p++)
-  //  b -= (n-i)<<2;
+  dir = n/2;
+  
+  //metodo da bissecao. Encontra a volta da espiral em que parou em log2 (n/2) iterações
+  while (1){
+    p = (dir+esq)/2;
+    pto = 4*(p*n - p*p);
+    ant = pto - 4*(n - (p-1)*2 - 1);
+    if (b>pto)
+      esq = p;
+    else if (b<pto && b<ant)
+      dir = p;
+    else if (b<=pto && b>ant){
+      i = p*2 + 1;
+      break;
+    }
+  }
+  b -= pto;
   i -= (p>0)?2:0;
   long int pos[] = {p,p-1};
-  printf("%ld %d b %ld tam %ld p %ld i %ld\n",pos[0],pos[1],b,n-i,p,i);
-
+  
   b += (n - i - 1);
   pos[0] += n - i - 1;
-  printf("%ld %d b %ld tam %ld p %ld i %ld\n",pos[0],pos[1],b,n-i,p,i);
   if (b>0){
     pos[0] -= b;
   }
   else{
     b += (n - i);
     pos[1] += n - i;
-    printf("%ld %d b %ld tam %ld p %ld i %ld\n",pos[0],pos[1],b,n-i,p,i);
     if (b>0){
       pos[1] -= b;
     }
     else{
       b += (n - i);
       pos[0] -= n - i;
-      printf("%ld %d b %ld tam %ld p %ld i %ld\n",pos[0],pos[1],b,n-i,p,i);
       if (b>0){
         pos[0] += b;
       }
